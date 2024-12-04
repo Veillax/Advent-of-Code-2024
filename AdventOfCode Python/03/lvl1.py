40205464da5f8fcac4e695eacb3b6249f039ac44
veillax1354 <lxhomeschool@gmail.com>
2024-12-03 21:56:08 -0700

    Added Day 03 code upon Day 04 puzzle release

--- AdventOfCode Python/03/lvl1.py
+++ AdventOfCode Python/03/lvl1.py
@@ -1,5 +1,8 @@
-import math
-import random
-import datetime
-import os
-import sys
+import re
+regex = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+f = open("input.txt", "r", encoding="utf-8")
+matches = re.findall(regex, f.read())
+digits = [list(map(int, m[4:-1].split(",")))  for m in matches]
+mul_digits = [digit[0] * digit[1] for digit in digits]
+total = sum(mul_digits)
+print(total)
--- AdventOfCode Python/03/lvl2.py
+++ AdventOfCode Python/03/lvl2.py
@@ -1,5 +1,19 @@
-import math
-import random
-import datetime
-import os
-import sys
+import re
+filename = 'input.txt'
+with open(filename, 'r', encoding="utf-8") as f:
+  content = f.read()
+regex = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+matches = re.findall(regex, content)
+total = 0
+enabled = True
+for match in matches:
+  index = content.rfind("do()", 0, content.index(match))
+  dont_index = content.rfind("don't()", 0, content.index(match))
+  if dont_index > index:
+    enabled = False
+  elif index > dont_index:
+    enabled = True
+  if enabled:
+    digits = [int(x) for x in match[4:-1].split(",")]
+    total += digits[0] * digits[1]
+print(total)
